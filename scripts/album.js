5a9ae3d2c27c8625d47e37f7910ecb0f181fbe11
pnb3431 <pete.bailen@gmail.com>
2016-08-30 02:06:45 +0000

    assignment 32 complete

--- scripts/album.js
+++ scripts/album.js
@@ -1,4 +1,10 @@
-
+var setSong = function(songNumber){
+    currentlyPlayingSongNumber = parseInt(songNumber);
+    currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
+     };
+var getSongNumberCell = function(number) {
+    return $('.song-item-number[data-song-number="' + number + '"]');
+};
  
 var createSongRow = function(songNumber, songName, songLength) {
      var template =
@@ -10,20 +16,20 @@ var createSongRow = function(songNumber, songName, songLength) {
       ;
  
      var $row = $(template);
+     
      var clickHandler = function() {
          	var songNumber = parseInt($(this).attr('data-song-number'));
          	
 
         if (currentlyPlayingSongNumber !== null) {
 		    // Revert to song number for currently playing song because user started playing new song.
-		    var currentlyPlayingCell = $('.song-item-number[data-song-number="' + currentlyPlayingSongNumber + '"]');
+		    var currentlyPlayingCell = getSongNumberCell(currentlyPlayingSongNumber);
 		    currentlyPlayingCell.html(currentlyPlayingSongNumber);
 	    }
-	    if (currentlyPlayingSongNumber !== songNumber) {
+	    if (currentlyPlayingSongNumber!== songNumber) {
 		    // Switch from Play -> Pause button to indicate new song is playing.
 		    $(this).html(pauseButtonTemplate);
-		    currentlyPlayingSongNumber = songNumber;
-		    currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
+		    setSong(songNumber);
 		    updatePlayerBarSong();
 	    } else if (currentlyPlayingSongNumber === songNumber) {
 		    // Switch from Pause -> Play button to pause currently playing song.
